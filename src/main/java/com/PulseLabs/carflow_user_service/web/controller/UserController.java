c4cef7ac79b9b8f628317b0aa1fd95b546ee004f
pierrick <pierrickviret@gmail.com>
2025-11-12 14:31:40 +0100

    V6 feat(APi) documentation of API

--- src/main/java/com/PulseLabs/carflow_user_service/web/controller/UserController.java
+++ src/main/java/com/PulseLabs/carflow_user_service/web/controller/UserController.java
@@ -2,12 +2,15 @@ package com.PulseLabs.carflow_user_service.web.controller;
 
 import com.PulseLabs.carflow_user_service.model.UserDTO;
 import com.PulseLabs.carflow_user_service.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Users", description = "API pour la gestion des utilisateurs/clients")
 @RestController
-@RequestMapping
+@RequestMapping("/Users")
 public class UserController {
     private final UserService userService;
 
@@ -16,39 +19,45 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/Users")
+    @Operation(summary = "Récupérer tous les utilisateurs", description = "Permet de récupérer la liste des utilisateurs avec filtres optionnels par nom et prénom")
+    @GetMapping
     public ResponseEntity<?> getClients(@RequestParam(value = "name", required = false) String name, @RequestParam(value = "surname" , required = false) String surname) {
         return ResponseEntity.ok(userService.getClients(name, surname));
-
     }
 
-    @PostMapping("/Users")
+    @Operation(summary = "Créer un utilisateur", description = "Permet de créer un nouvel utilisateur")
+    @PostMapping
     public ResponseEntity<?> createClient(@RequestBody UserDTO user) {
         return ResponseEntity.ok(userService.saveClient(user));
     }
 
-
-    @PutMapping("/Users")
+    @Operation(summary = "Mettre à jour un utilisateur", description = "Permet de mettre à jour les informations d'un utilisateur existant")
+    @PutMapping
     public ResponseEntity<?> updateClient(@RequestBody UserDTO user) {
         return ResponseEntity.ok(userService.updateClient(user));
     }
 
-    @GetMapping( "/Users/{id}")
+    @Operation(summary = "Récupérer un utilisateur par ID", description = "Permet de récupérer les détails d'un utilisateur spécifique")
+    @GetMapping("/{id}")
     public ResponseEntity<?> afficherUnClient(@PathVariable int id) {
         return ResponseEntity.ok(userService.getClientById(id));
     }
-    @DeleteMapping( "/Users/{id}")
+
+    @Operation(summary = "Supprimer un utilisateur", description = "Permet de supprimer un utilisateur par son ID")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteClient(@PathVariable int id) {
         userService.deleteClient(id);
         return ResponseEntity.ok("Client supprimé");
     }
 
-    @GetMapping( "/Users/{id}/age")
+    @Operation(summary = "Récupérer l'âge d'un utilisateur", description = "Permet de récupérer l'âge d'un utilisateur par son ID")
+    @GetMapping("/{id}/age")
     public ResponseEntity<?> getAgeOfClient(@PathVariable int id) {
         return ResponseEntity.ok(userService.getClientAgeWithId(id));
     }
 
-    @GetMapping( "/Users/{id}/validityLicense")
+    @Operation(summary = "Vérifier la validité du permis", description = "Permet de vérifier la validité du permis de conduire d'un utilisateur")
+    @GetMapping("/{id}/validityLicense")
     public ResponseEntity<?> getValidityLicense(@PathVariable int id) {
         return ResponseEntity.ok(userService.getValidityLicense(id));
     }
