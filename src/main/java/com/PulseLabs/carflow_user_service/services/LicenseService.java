6c88aa7a64cc8c2ad31ff1e2b53095669e4ff65b
pierrick <pierrickviret@gmail.com>
2025-11-17 13:48:02 +0100

    V6 bug(api) update license adress

--- src/main/java/com/PulseLabs/carflow_user_service/services/LicenseService.java
+++ src/main/java/com/PulseLabs/carflow_user_service/services/LicenseService.java
@@ -3,27 +3,29 @@ package com.PulseLabs.carflow_user_service.services;
 import com.PulseLabs.carflow_user_service.Error.ServeurDriverLicenseNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.time.Duration;
+import java.util.List;
+
 
 @Service
 public class LicenseService {
-    private final RestTemplate restTemplate;
-
-    @Value("${license.service.url:http://192.168.1.87:8084}")
-    private String licenseServiceUrl;
-
-    public LicenseService() {
-        this.restTemplate = new RestTemplate();
-    }
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     public boolean isDriveLicenseValide(String driveLicense) {
         try {
-            String url = licenseServiceUrl + "/licenses/" + driveLicense;
-            String response = restTemplate.getForObject(url, String.class);
-            return "true".equals(response);
+            Boolean response = webClientBuilder.build()
+                    .get()
+                    .uri("http://DRIVERLICENSESERVICE/licenses/"+ driveLicense)
+                    .retrieve()
+                    .bodyToMono(Boolean.class)
+                    .block(Duration.ofSeconds(5));
+            return response;
         } catch (Exception e) {
             throw new ServeurDriverLicenseNotFound("Error checking license: " + e.getMessage());
         }
