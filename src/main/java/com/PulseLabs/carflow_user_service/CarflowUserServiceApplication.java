ed84da9ad838c30ad0c193c15140e9da46257dcf
pierrick <pierrickviret@gmail.com>
2025-11-19 09:56:47 +0100

    V6 bug(webclient) change to restTemplate

--- src/main/java/com/PulseLabs/carflow_user_service/CarflowUserServiceApplication.java
+++ src/main/java/com/PulseLabs/carflow_user_service/CarflowUserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 
@@ -22,5 +23,11 @@ public class CarflowUserServiceApplication {
     @LoadBalanced
     public WebClient.Builder loadBalancedWebClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- src/main/java/com/PulseLabs/carflow_user_service/services/LicenseService.java
+++ src/main/java/com/PulseLabs/carflow_user_service/services/LicenseService.java
@@ -4,6 +4,7 @@ import com.PulseLabs.carflow_user_service.Error.ServeurDriverLicenseNotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -17,6 +18,20 @@ public class LicenseService {
     @Autowired
     private WebClient.Builder webClientBuilder;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    public boolean isDriveLicenseValide(String driveLicense) {
+        try {
+            ResponseEntity<Boolean> response = restTemplate.getForEntity("http://DRIVERLICENSESERVICE/licenses/"+ driveLicense, Boolean.class);
+            return response.getBody();
+        } catch (Exception e) {
+            throw new ServeurDriverLicenseNotFound("Error checking license: " + e.getMessage());
+        }
+    }
+
+    /*
     public boolean isDriveLicenseValide(String driveLicense) {
         try {
             Boolean response = webClientBuilder.build()
@@ -30,5 +45,5 @@ public class LicenseService {
             throw new ServeurDriverLicenseNotFound("Error checking license: " + e.getMessage());
         }
     }
-
+*/
 }
